2c7db5ed50346228248df649f42fe082e59af4fa
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2025-07-10 16:58:54 +0330

    conf: gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 export default function App() {
   return <div className="text-2xl text-green-500 font-bold">App</div>;
 }
